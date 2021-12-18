32fe56605245062305a9c825750c3bff2dc01726
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2021-12-18 17:58:35 +0530

    Validation between start time and end time done

--- app/src/main/java/com/sayantanbanerjee/interviewcreationportal/MeetingActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/interviewcreationportal/MeetingActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -14,11 +15,20 @@ import java.util.*
 class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     TimePickerDialog.OnTimeSetListener {
 
+    // Initialize all the variables which holds the views
     private lateinit var chooseDateButton: Button
     private lateinit var chooseStartTimeButton: Button
     private lateinit var chooseEndTimeButton: Button
 
+    // Initialize all the variables which holds date and time
     var startTimeClicked: Boolean = false
+    var dayChosen = -1
+    var monthChosen = -1
+    var yearChosen = -1
+    var startHour = -1
+    var startMinute = -1
+    var endHour = -1
+    var endMinute = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,36 +53,52 @@ class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
 
         // Code to choose the start time from time picker dialog.
         chooseStartTimeButton.setOnClickListener {
-            startTimeClicked = true
-            val calendarStartTime: Calendar = Calendar.getInstance()
-            val timePickerDialog = TimePickerDialog(
-                this@MeetingActivity,
-                this@MeetingActivity,
-                calendarStartTime.get(Calendar.HOUR),
-                calendarStartTime.get(Calendar.MINUTE),
-                true
-            )
-            timePickerDialog.show()
+            if (dayChosen == -1) {
+                Toast.makeText(this, "First SELECT Date", Toast.LENGTH_LONG).show()
+            } else {
+                startTimeClicked = true
+                val calendarStartTime: Calendar = Calendar.getInstance()
+                val timePickerDialog = TimePickerDialog(
+                    this@MeetingActivity,
+                    this@MeetingActivity,
+                    calendarStartTime.get(Calendar.HOUR),
+                    calendarStartTime.get(Calendar.MINUTE),
+                    true
+                )
+                timePickerDialog.show()
+            }
+
         }
 
         // Code to choose the end time from time picker dialog.
         chooseEndTimeButton.setOnClickListener {
-            startTimeClicked = false
-            val calendarEndTime: Calendar = Calendar.getInstance()
-            val timePickerDialog = TimePickerDialog(
-                this@MeetingActivity,
-                this@MeetingActivity,
-                calendarEndTime.get(Calendar.HOUR),
-                calendarEndTime.get(Calendar.MINUTE),
-                true
-            )
-            timePickerDialog.show()
+            if (startHour == -1) {
+                Toast.makeText(
+                    this,
+                    "Choose Start Time before choosing End Time!",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                startTimeClicked = false
+                val calendarEndTime: Calendar = Calendar.getInstance()
+                val timePickerDialog = TimePickerDialog(
+                    this@MeetingActivity,
+                    this@MeetingActivity,
+                    calendarEndTime.get(Calendar.HOUR),
+                    calendarEndTime.get(Calendar.MINUTE),
+                    true
+                )
+                timePickerDialog.show()
+            }
         }
 
     }
 
     // Setting of the chosen date from the date picker dialog
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        yearChosen = year
+        monthChosen = month
+        dayChosen = dayOfMonth
         val dateText: String = "DATE : $dayOfMonth  /  $month  /  $year"
         chooseDateButton.text = dateText
     }
@@ -82,10 +108,24 @@ class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
         val timeText: String = "$hourOfDay : $minute"
         if (startTimeClicked) {
+            startHour = hourOfDay
+            startMinute = minute
             chooseStartTimeButton.text = "START : $timeText"
         } else {
-            chooseEndTimeButton.text = "END : $timeText"
+            if (startHour > hourOfDay || (startHour == hourOfDay && startMinute >= minute)) {
+                Toast.makeText(
+                    this,
+                    "Start Time cannot be greater than End Time!",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                endHour = hourOfDay
+                endMinute = minute
+                chooseEndTimeButton.text = "END : $timeText"
+            }
+
         }
+
     }
 
 
