7f0bfcc09d844b7b61eead237a5be0de2b6aeaf2
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2021-12-18 12:15:15 +0530

    Created the UI for Add User Screen

--- app/src/main/java/com/sayantanbanerjee/interviewcreationportal/MainActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/interviewcreationportal/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.sayantanbanerjee.interviewcreationportal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+// The activity class of the base screen, where all the scheduled interviews displayed,
+// and also have two FAB (floating action button) routing user to Add User Activity and Add/Update Meeting Activity.
 class MainActivity : AppCompatActivity() {
+
+    // Variable binding Add User Floating Action Button view to it
+    private lateinit var addUserButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Route User To ADD USER activity, when user clicks on ADD USER Floating Action Button.
+        addUserButton = findViewById(R.id.fabAddUser)
+        addUserButton.setOnClickListener {
+            val intent = Intent(this, AddUserActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
