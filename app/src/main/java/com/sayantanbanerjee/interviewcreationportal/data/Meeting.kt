4b55789acffa430fa93865a73947faa6bcda25be
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2021-12-18 09:26:44 +0530

    Create data classes

---
+++ app/src/main/java/com/sayantanbanerjee/interviewcreationportal/data/Meeting.kt
@@ -0,0 +1,17 @@
+package com.sayantanbanerjee.interviewcreationportal.data
+
+/**
+ * Generalized structure of a meeting.
+ *
+ * @param id: Unique ID of the meeting.
+ * @param name: Name of the meeting.
+ * @param slot: Slot (startStamp and endStamp) of the meeting.
+ * @param users: List of users present in the meeting.
+ */
+
+data class Meeting(
+        val id : String,
+        val name : String,
+        val slot: Slot,
+        val users : List<String>
+)
