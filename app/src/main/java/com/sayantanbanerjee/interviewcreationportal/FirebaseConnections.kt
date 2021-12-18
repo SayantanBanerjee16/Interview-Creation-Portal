16dfb0b587a4c168335ad3d43978796ae1449043
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2021-12-18 19:49:35 +0530

    Initial meeting slot setup in firebase done

--- app/src/main/java/com/sayantanbanerjee/interviewcreationportal/FirebaseConnections.kt
+++ app/src/main/java/com/sayantanbanerjee/interviewcreationportal/FirebaseConnections.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.widget.Toast
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.sayantanbanerjee.interviewcreationportal.data.Meeting
+import com.sayantanbanerjee.interviewcreationportal.data.Slot
 import com.sayantanbanerjee.interviewcreationportal.data.User
 
 // This class is used to make the app connection with the firebase realtime database.
@@ -17,10 +19,23 @@ class FirebaseConnections {
             val userId = System.currentTimeMillis().toString()
             val user = User(userId, name, email)
             // Upload user class to firebase
-            reference.child("users").child(userId).setValue(user)
+            reference.child(context.getString(R.string.users)).child(userId).setValue(user)
             // Display Toast to user
             Toast.makeText(
                 context, context.getString(R.string.user_uploaded_to_firebase), Toast.LENGTH_LONG
+            ).show()
+        }
+
+        fun uploadMeetingToFirebase(context: Context, meetingName: String, timestampStart: String, timestampEnd: String) {
+            val reference = Firebase.database.reference
+            // create the Meeting class
+            val meetingId = System.currentTimeMillis().toString()
+            val meeting = Meeting(meetingId, meetingName, Slot(timestampStart, timestampEnd))
+            // Upload meeting class to firebase
+            reference.child(context.getString(R.string.meeting)).child(meetingId).setValue(meeting)
+            // Display Toast to user
+            Toast.makeText(
+                context, context.getString(R.string.meeting_uploaded_to_firebase), Toast.LENGTH_LONG
             ).show()
         }
 
--- app/src/main/java/com/sayantanbanerjee/interviewcreationportal/MeetingActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/interviewcreationportal/MeetingActivity.kt
@@ -1,14 +1,21 @@
+@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
 package com.sayantanbanerjee.interviewcreationportal
 
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.util.*
 
 // This activity is responsible for creation / updation of a meeting.
@@ -16,9 +23,11 @@ class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     TimePickerDialog.OnTimeSetListener {
 
     // Initialize all the variables which holds the views
+    private lateinit var meetingName: TextInputLayout
     private lateinit var chooseDateButton: Button
     private lateinit var chooseStartTimeButton: Button
     private lateinit var chooseEndTimeButton: Button
+    private lateinit var addMeetingButton: Button
 
     // Initialize all the variables which holds date and time
     var startTimeClicked: Boolean = false
@@ -29,13 +38,17 @@ class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     var startMinute = -1
     var endHour = -1
     var endMinute = -1
+    var timeStampStart = ""
+    var timeStampEnd = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meeting)
+        meetingName = findViewById(R.id.nameOfMeeting)
         chooseDateButton = findViewById(R.id.chooseDatePicker)
         chooseStartTimeButton = findViewById(R.id.chooseStartTimePicker)
         chooseEndTimeButton = findViewById(R.id.chooseEndTimePicker)
+        addMeetingButton = findViewById(R.id.addMeetingButton)
 
         // Code to choose the date from date picker dialog.
         chooseDateButton.setOnClickListener {
@@ -54,7 +67,11 @@ class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
         // Code to choose the start time from time picker dialog.
         chooseStartTimeButton.setOnClickListener {
             if (dayChosen == -1) {
-                Toast.makeText(this, "First SELECT Date", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "First Select Date before selecting start time!",
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
                 startTimeClicked = true
                 val calendarStartTime: Calendar = Calendar.getInstance()
@@ -92,14 +109,75 @@ class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
             }
         }
 
+        // On clicking the button to add meeting, it will first validate the input fields,
+        // and upon passing validation, it will add the meeting on the firebase.
+        addMeetingButton.setOnClickListener {
+            if (validationOfFields()) {
+                if (NetworkConnectivity.isNetworkAvailable(this)) {
+                    // If network connectivity present, then update the meeting to server.
+                    val nameOfMeeting = meetingName.editText?.text.toString()
+                    parseDateTimeToTimeStamp()
+                    FirebaseConnections.uploadMeetingToFirebase(
+                        this,
+                        nameOfMeeting,
+                        timeStampStart,
+                        timeStampEnd
+                    )
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        // close the activity after 0.5 second
+                        finish()
+                    }, 500)
+                } else {
+                    Toast.makeText(this, getString(R.string.no_network), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }
+
+    private fun validationOfFields(): Boolean {
+        val nameOfMeeting = meetingName.editText?.text.toString()
+        if (nameOfMeeting == "") {
+            Toast.makeText(
+                this,
+                "Choose Name!",
+                Toast.LENGTH_LONG
+            ).show()
+            return false
+        }
+
+        if (endHour == -1) {
+            Toast.makeText(
+                this,
+                "Recheck all Date and Time fields!",
+                Toast.LENGTH_LONG
+            ).show()
+            return false
+        }
+
+        return true
+    }
+
+    private fun parseDateTimeToTimeStamp() {
+
+        val dateTimeStart =
+            LocalDateTime.of(yearChosen, monthChosen, dayChosen, startHour, startMinute, 0)
+        timeStampStart =
+            dateTimeStart.atZone(ZoneOffset.ofHoursMinutes(5, 30)).toEpochSecond().toString()
+
+        val dateTimeEnd =
+            LocalDateTime.of(yearChosen, monthChosen, dayChosen, endHour, endMinute, 0)
+        timeStampEnd =
+            dateTimeEnd.atZone(ZoneOffset.ofHoursMinutes(5, 30)).toEpochSecond().toString()
+
     }
 
     // Setting of the chosen date from the date picker dialog
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
         yearChosen = year
-        monthChosen = month
+        monthChosen = month + 1
         dayChosen = dayOfMonth
-        val dateText: String = "DATE : $dayOfMonth  /  $month  /  $year"
+        val dateText: String = "DATE : $dayOfMonth  /  $monthChosen  /  $year"
         chooseDateButton.text = dateText
     }
 
@@ -108,9 +186,24 @@ class MeetingActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
         val timeText: String = "$hourOfDay : $minute"
         if (startTimeClicked) {
-            startHour = hourOfDay
-            startMinute = minute
-            chooseStartTimeButton.text = "START : $timeText"
+            if (endHour != -1) {
+                if (endHour < hourOfDay || (endHour == hourOfDay && endMinute <= minute)) {
+                    Toast.makeText(
+                        this,
+                        "Start Time cannot be lesser than End Time!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    startHour = hourOfDay
+                    startMinute = minute
+                    chooseStartTimeButton.text = "START : $timeText"
+                }
+            } else {
+                startHour = hourOfDay
+                startMinute = minute
+                chooseStartTimeButton.text = "START : $timeText"
+            }
+
         } else {
             if (startHour > hourOfDay || (startHour == hourOfDay && startMinute >= minute)) {
                 Toast.makeText(
